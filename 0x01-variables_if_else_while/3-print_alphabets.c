000c5ce07bc9e26ada891ec9fab3bfa0927692f3
ezakariaa <zakariaorche@gmail.com>
2023-07-14 16:56:51 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,37 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible combinations
+ * main - program that prints the alphabet in lowercase,
  * followed by a new line
- * description: 'print combination'
+ * description: 'alphabet in lower case'
  * Return: always 0
  */
 
 int main(void)
 {
-	int digit1 = 0, digit2;
+	int n = 97;
 
-	while (digit1 <= 9)
+	while (n < 123)
 	{
-		digit2 = 0;
-		while (digit2 <= 9)
-		{
-			if (digit1 != digit2 && digit1 < digit2)
-			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
-
-				if (digit1 + digit2 != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			digit2++;
-		}
-		digit1++;
+		putchar(n);
+		n++;
 	}
 	putchar('\n');
 	return (0);
 }
-

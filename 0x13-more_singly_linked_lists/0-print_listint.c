8dd8ca0a9b0fa067dce1581ebf875e86ef469bd1
ezakariaa <zakariaorche@gmail.com>
2023-08-29 17:58:25 -0700

    Free (safe version)

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,6 +4,7 @@
 /**
 * print_listint - Prints all elements of a listint_t list.
 * @h: Pointer to the head of list_t list.
+* 
 * Return: number of nodes.
 */
 size_t print_listint(const listint_t *h)
@@ -12,11 +13,13 @@ size_t print_listint(const listint_t *h)
 
 	if (h == NULL)
 		return (0);
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		nbrnode++;
 	}
+
 	return (nbrnode);
 }

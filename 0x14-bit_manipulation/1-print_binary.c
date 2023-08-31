7968c973e5ae05f003ddcefc5e482daf6cfae694
ezakariaa <zakariaorche@gmail.com>
2023-08-31 12:57:13 -0700

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_binary - prints the binary representation of a number.
+* not allowed to use arrays
+* not allowed to use malloc
+* not allowed to use the % or / operators
+*
+* Return: Always 0.
+*/
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
+}

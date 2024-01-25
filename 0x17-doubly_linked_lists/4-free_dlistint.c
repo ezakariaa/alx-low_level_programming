dc8dbd3884242662ad18cf8e5c4b245a02090315
ezakariaa <zakariaorche@gmail.com>
2024-01-25 07:04:00 -0800

    4. Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_dlistint - function that frees a dlistint_t list.
+* @head: the head node of the list.
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}

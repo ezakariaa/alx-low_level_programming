77e59af6509aba1a3a8c6039de36f60a775cb1c2
ezakariaa <zakariaorche@gmail.com>
2024-01-29 13:28:35 -0800

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,13 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdlib.h>
-
-/*
- * File: main.h
- * Desc: Header file contains all the function prototypes
- *
- */
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -30,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /*_MAIN_H_*/

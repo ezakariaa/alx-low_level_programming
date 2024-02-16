bc2aa47588fc4061c4ca782ed2c09f1402bdeb27
ezakariaa <zakariaorche@gmail.com>
2024-02-16 12:27:39 -0800

    2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,14 +2,13 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Computes the index of a key in a hash table array
- * @key: The key string
- * @size: The size of the hash table array
- *
- * Return: The index at which the key/value pair should be stored
- */
+* key_index - Computes the index of a key in a hash table array
+* @key: The key string
+* @size: The size of the hash table array
+* Return: The index at which the key/value pair should be stored
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value = hash_djb2(key);
-    return (hash_value % size);
+	unsigned long int hash_value = hash_djb2(key);
+	return (hash_value % size);
 }

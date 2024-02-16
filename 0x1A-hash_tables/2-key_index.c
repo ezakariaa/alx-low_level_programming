2af18b9c61a9e31eb26591fa0589f335ca62ffe4
ezakariaa <zakariaorche@gmail.com>
2024-02-16 12:46:37 -0800

    2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,6 +9,8 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_value = hash_djb2(key);
+	unsigned long int hash_value;
+
+	hash_value = hash_djb2(key);
 	return (hash_value % size);
 }

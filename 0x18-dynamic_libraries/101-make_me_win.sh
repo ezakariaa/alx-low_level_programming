1e7f009fad9858e95af720cbda0a5251ef4ca365
ezakariaa <zakariaorche@gmail.com>
2024-02-16 03:24:11 -0800

    0. >>> ht = {}

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/ezakariaa/alx-low_level_programming/ba9ca826c86f5f171e615e11c5cd17d29f4a903b/0x18-dynamic_libraries/libhack.so
+wget -P .. https://raw.githubusercontent.com/ezakariaa/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
